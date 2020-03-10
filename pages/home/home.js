58b62904496d1b983d5ccdf1f3980c7b3e7d055d
yanglt7 <yanglt7@qq.com>
2020-03-11 00:09:46 +0800

    事件处理

--- pages/home/home.js
+++ pages/home/home.js
@@ -5,62 +5,72 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    titles: ['衣服', '裤子', '鞋子']
+  },
+  handleClick() {
+    console.log('按钮发生了点击')
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  // 常见的一些事件
+  handleTouchStart() {
+    console.log('handleTouchStart')
+  },
+  handleTouchMove() {
+    console.log('handleTouchMove')
+  },
+  handleTouchEnd() {
+    console.log('handleTouchEnd')
+  },
+  handleTag() {
+    console.log('handleTag')
+  },
+  handleLongpress() {
+    console.log('handleLongpress')
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  // 事件对象分析
+  handleEventClick(event) {
+    console.log('--------', event)
+  },
+  handleEventEnd(event) {
+    console.log('+++++++', event)
+  },
+  handleInner(event) {
+    console.log('handleInner', event)
+  },
+  handleOuter(event) {
+    console.log('handleOuter', event)
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
+  // 事件的传递参数
+  handleItemClick(event) {
+    console.log('handleItemClick', event)
+    // title - index
+    const dataset = event.currentTarget.dataset
+    const title = dataset.item
+    const index = dataset.index
 
+    console.log(index, title)
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
+  // 事件冒泡和事件捕获
+  handleCaptureView1() {
+    console.log('handleCaptureView1')
+  },
+  handleBindView1() {
+    console.log('handleBindView1')
+  },
+  handleCaptureView2() {
+    console.log('handleCaptureView2')
+  },
+  handleBindView2() {
+    console.log('handleBindView2')
+  },
+  handleCaptureView3() {
+    console.log('handleCaptureView3')
+  },
+  handleBindView3() {
+    console.log('handleBindView3')
   },
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })
