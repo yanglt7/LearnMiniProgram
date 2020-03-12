465ddf63a37ee1ddf2c9d25b4fa84fa7d365e046
yanglt7 <yanglt7@qq.com>
2020-03-12 23:27:04 +0800

    展示弹窗和使用分享

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,81 @@
 // pages/home/home.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
+  handleShowToast() {
+    wx.showToast({
+      title: '加载中',
+      duration: 3000,
+      icon: 'loading',
+      mask: true,
+      success: () => {
+        console.log('展示弹窗成功')
+      },
+      fail: () => {
+        console.log('展示弹窗失败')
+      }, 
+      complete: () => {
+        console.log('完成函数的调用')
+      }
+    })
+  },
+  
+  handleShowModal() {
+    wx.showModal({
+      title: '标题',
+      content: '内容',
+      // showCancel: false,
+      cancelText: '退出',
+      cancelColor: 'grey',
+      confirmColor: 'green',
+      success: res => {
+        console.log(res)
+        if (res.cancel) {
+          console.log('用户点击了取消按钮')
+        }
+        if (res.confirm) {
+          console.log('用户点击了确定按钮')
+        }
+      }
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
+  // 和showToast区别: 必须手动调用hideLoading函数
+  handleShowLoading() {
+    wx.showLoading({
+      title: '全力加载中',
+      mask: true
+    })
 
+    // 一般在发送网络请求时使用
+    setTimeout(() => {
+      wx.hideLoading()
+    }, 3000)
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  handleShowActionSheet() {
+    wx.showActionSheet({
+      itemList: ['相册', '拍照'],
+      itemColor: 'red',
+      success: function(res) {
+        console.log(res)
+        switch(res.tapIndex) {
+          case 0: 
+            console.log('用户选择了相册')
+            break;
+          case 1:
+            console.log('用户选择了拍照')
+            break;
+        }
+      },
+      fail: function(res) {},
+      complete: function(res) {},
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  onShareAppMessage: function(options) {
+    return {
+      title: 'hihihi',
+      path: '/pages/about/about',
+      imgUrl: ''
+    }
   }
 })
