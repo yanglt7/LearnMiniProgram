d9d000dfa3a1b7067afcd6afdb97b4fee3b0c1ff
yanglt7 <yanglt7@qq.com>
2020-03-12 22:00:41 +0800

    网络请求

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,52 @@
 // pages/home/home.js
+import request from '../../services/network.js'
+
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
   onLoad: function (options) {
+    // 1.原生的方式发送网络请求
+    // this.get_data_origin()
 
+    // 2.使用封装的request发送网络请求
+    request({
+      url: 'http://123.207.32.32:8000/recommend',
+    }).then(res => {
+      console.log(res)
+    }).catch (err => {
+      console.log(err)
+    })
   },
+  get_data_origin() {
+    // 1.发送最简单的get请求
+    wx.request({
+      url: 'http://123.207.32.32:8000/recommend',
+    })
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
+    // 2.发送get请求, 携带参数
+    wx.request({
+      url: 'http://123.207.32.32:8000/home/data',
+      data: {
+        type: 'sell',
+        page: 1
+      },
+      success: res => {
+        console.log(res)
+      }
+    })
 
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+    // 3.post请求, 携带参数
+    wx.request({
+      url: 'http://httpbin.org/post',
+      method: 'post',
+      data: {
+        name: 'yanglt',
+        age: 18
+      },
+      success: res => {
+        console.log(res)
+      },
+      fail: err => {
+        console.log(err)
+      }
+    })
   }
 })
